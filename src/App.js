d3f30ecbbbea83d27724a05bac83bf8e831f4eb8
LJXSean <risingsun221@gmail.com>
2023-08-27 21:55:22 +0800

    Change browserRouter to hashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/landing/Landing";
 import Login from "./components/auth/Login";
