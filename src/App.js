caab093f7c3515e7b75aea556a961afafb80f955
LJXSean <risingsun221@gmail.com>
2023-08-27 21:58:21 +0800

    Update router basename

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ const App = () => {
   }, []);
   return (
     <Provider store={store}>
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router>
         <Alert />
         <Navbar />
         <Routes>
